9a962427f1205dca84a4dd237986660a0ea22fce
isjmovo <isjmovo@gmail.com>
2024-02-28 08:54:35 +0900

    archive 엔티티 클래스 생성

---
+++ src/main/java/com/toy/dev/archive/recode/Archive.java
@@ -0,0 +1,25 @@
+package com.toy.dev.archive.recode;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+public class Archive {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer arcId;
+
+  @Column(length = 200)
+  private String title;
+
+  private Integer price;
+
+  @CreationTimestamp
+  private LocalDateTime insertDateTime;
+}
