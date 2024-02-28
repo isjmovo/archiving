e3e92c3847850e736806d352cc200d5a049578ea
isjmovo <isjmovo@gmail.com>
2024-02-28 09:38:44 +0900

    archiveRepository

---
+++ src/main/java/com/toy/dev/archive/recode/ArchiveRepository.java
@@ -0,0 +1,6 @@
+package com.toy.dev.archive.recode;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArchiveRepository extends JpaRepository<Archive, Integer> {
+}
