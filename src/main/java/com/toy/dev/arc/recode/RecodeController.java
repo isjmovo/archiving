5e90dea0be1e102a9881eeca8e04e1f818212018
isjmovo <isjmovo@gmail.com>
2024-02-28 10:05:18 +0900

    아카이브 입력 컨트롤러 메소드

--- src/main/java/com/toy/dev/arc/recode/RecodeController.java
+++ src/main/java/com/toy/dev/arc/recode/RecodeController.java
@@ -1,12 +1,24 @@
 package com.toy.dev.arc.recode;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RecodeController {
+  @Autowired
+  private RecodeService recodeService;
+
   @GetMapping("/rec/create")
   public String create() {
     return "rec/create";
+  }
+
+  @PostMapping("/book/create")
+  public String insert(ArcCreateDTO arcCreateDTO) {
+    Integer arcId = recodeService.insert(arcCreateDTO);
+
+    return String.format("redirect:/rec/read/%s", arcId);
   }
 }
